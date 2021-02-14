cb2b5600d4b72c0218aff9aeb39acafe886e0e68
MityaDementiy <d.dementiy@gmail.com>
2021-02-14 15:18:41 +0200

    Render messages with user name

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import _ from 'lodash';
 
-const renderMessage = (message) => {
-   const getNextId = () => Number(_.uniqueId());
-
-   return <p key={getNextId()}>{message}</p>
-};
-
-const App = () => {
-   const [messages, setMessages] = useState(['Привет', 'Как дела?']);
-
-   const handleAnswer = () => {
-      setMessages([...messages, 'Нормально']);
-   }
-
-   return (
-      <>
-        {messages && messages.map(renderMessage)}
-        <button onClick={handleAnswer}>Ответить</button>
-      </>
-   );
-};
+import App from './components/MessageField.jsx';
 
 ReactDOM.render(
    <App />,
